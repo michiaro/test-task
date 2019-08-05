7f0019ab714b17f5e1c49126e634b29d604836b3
Michiaro <michiaro@bk.ru>
2019-08-05 21:54:26 +0500

    rework card logic

--- src/index.js
+++ src/index.js
@@ -1,4 +1,25 @@
-import "normalize.css";
+const cardNodes = document.querySelectorAll("[data-visible]");
+const buttonNode = document.getElementById("button");
+const messageNode = document.getElementById("message");
+const CARDS_PER_CLICK = 5;
 
-import "./styles/main.scss";
-import "./js/show-cards.js";
+buttonNode.addEventListener(
+  "click",
+  function() {
+    let counter = 0;
+
+    for (let i = 0; i < cardNodes.length; i++) {
+      if (cardNodes[i].dataset.visible === "false" && counter <= CARDS_PER_CLICK) {
+        cardNodes[i].dataset.visible = "true";
+        counter++;
+      }
+    }
+
+    let visibleCards = document.querySelectorAll("[data-visible='true']");
+    if (visibleCards.length == cardNodes.length) {
+      buttonNode.style.display = "none";
+      messageNode.style.display = "block";
+    }
+  },
+  false
+);
